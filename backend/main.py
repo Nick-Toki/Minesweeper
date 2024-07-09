cdbdad72019c5268c7d5653ac9884238db294258
Cedrigo <fireflyz999@icloud.com>
2024-07-09 11:13:55 +0200

    frontend changes

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,10 @@
 from flask import Flask, Response, request
+from flask_cors import CORS
 import Minesweeper
 import json
 
-
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def index():
--- frontend/Praktikum Projekt/js/javascript.js
+++ frontend/Praktikum Projekt/js/javascript.js
@@ -1,18 +1,42 @@
-$(document).ready(function() {
-    $('#btn').click(function() {
-        $.ajax({
-            url: 'positions.json',
-            method: 'GET',
-            dataType: 'json',
-            success: function(data) {
-                generateField(data.positions);
-            },
-            error: function(jqXHR, textStatus, errorThrown) {
-                console.error('Fehler: ' + textStatus, errorThrown);
-            }
-        });
+// value -1: Mine
+// value 0: Leeres Feld
+// value 1-8: Feld Nr. 1-8
+
+// $(document).ready(function() {
+//     $('#btn').click(function() {
+//         $.ajax({
+//             url: 'http://localhost:81/',
+//             method: 'GET',
+//             dataType: 'json',
+//             success: function(data) {
+//                 generateField(data);
+//                 console.log(data)
+//             },
+//             error: function(jqXHR, textStatus, errorThrown) {
+//                 console.error('Fehler beim Abrufen der Daten: ' + textStatus, errorThrown);
+//                 console.error('Status Code:', jqXHR.status);
+//                 console.error('Antworttext:', jqXHR.responseText);
+//             }
+//         });
+//     });
+// });
+ 
+function getField(){
+    $.ajax({
+        url: 'http://localhost:81/',
+        method: 'GET',
+        dataType: 'json',
+        success: function(data) {
+            generateField(data);
+            console.log(data)
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.error('Fehler beim Abrufen der Daten: ' + textStatus, errorThrown);
+            console.error('Status Code:', jqXHR.status);
+            console.error('Antworttext:', jqXHR.responseText);
+        }
     });
-});
+}
 
 function rightClickFlag () {
     var rcfElements = document.querySelectorAll(".rcFlag");
@@ -28,44 +52,46 @@ function rightClickFlag () {
             if (currentState === "default" && this.src.includes("tile.png")) {
                 this.src = "./img/flagtile.png";
                 this.dataset.currentState = "flag";
+                this.position[i][j]["Flagge"] === true;
             }
 
             else if (currentState === "flag" && this.src.includes("flagtile.png")) {
                 this.src = "./img/tile.png";
                 this.dataset.currentState = "default";
+                this.position[i][j]["Flagge"] === false;
             }
         });
     });
 }
 
-let exPosition = [
-    [{},{},{},{},{},{},{},{},{},{}],
-    [{},{},{},{},{},{},{},{},{},{}],
-    [{},{},{},{},{},{},{},{},{},{}],
-    [{},{},{},{},{},{},{},{},{},{}],
-    [{},{},{},{},{},{},{},{},{},{}],
-    [{},{},{},{},{},{},{},{},{},{}],
-    [{},{},{},{},{},{},{},{},{},{}],
-    [{},{},{},{},{},{},{},{},{},{}],
-    [{},{},{},{},{},{},{},{},{},{}],
-    [{},{},{},{},{},{},{},{},{},{}],
-];
+// let exPosition = [
+//     [{},{},{},{},{},{},{},{},{},{}],
+//     [{},{},{},{},{},{},{},{},{},{}],
+//     [{},{},{},{},{},{},{},{},{},{}],
+//     [{},{},{},{},{},{},{},{},{},{}],
+//     [{},{},{},{},{},{},{},{},{},{}],
+//     [{},{},{},{},{},{},{},{},{},{}],
+//     [{},{},{},{},{},{},{},{},{},{}],
+//     [{},{},{},{},{},{},{},{},{},{}],
+//     [{},{},{},{},{},{},{},{},{},{}],
+//     [{},{},{},{},{},{},{},{},{},{}],
+// ];
 
-function randBool(threshold) {
-    return Math.random() >= threshold;
-}
+// function randBool(threshold) {
+//     return Math.random() >= threshold;
+// }
 
-function randValue(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+// function randValue(min, max) {
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
 
-for (let i = 0; i < exPosition.length; i++) {
-    for (let j = 0; j < exPosition[i].length; j++) {
-        exPosition[i][j]["Mine"] = randBool(0.95);
-        exPosition[i][j]["Offen"] = randBool(0.1);
-        exPosition[i][j]["Value"] = randValue(1, 8);
-    }
-}
+// for (let i = 0; i < exPosition.length; i++) {
+//     for (let j = 0; j < exPosition[i].length; j++) {
+//         exPosition[i][j]["Mine"] = randBool(0.95);
+//         exPosition[i][j]["Offen"] = randBool(0.1);
+//         exPosition[i][j]["Value"] = randValue(1, 8);
+//     }
+// }
 
 function generateField(position) {
     let field = document.getElementById("gameField");
@@ -79,20 +105,24 @@ function generateField(position) {
             var img = document.createElement("img");
             img.classList.add("rcFlag");
 
-            if (position[i][j]["Mine"] === true) {
+            if (position[i][j]["Mine"] === true && (position[i][j]["Value"] === -1)) {
                 img.src = "./img/tilebomb.png";
             } 
             
-            else if (position[i][j]["Offen"] === true) {
-                img.src = "./img/tileempty.png";
+            else if (position[i][j]["Offen"] === true && position[i][j]["Value"] !== 0) {
+                img.src = `./img/sweepertile${position[i][j]["Value"]}.png`;
             } 
+
+            else if (position[i][j]["Offen"] === true && position[i][j]["Value"] === 0) {
+                img.src = "./img/tileempty.png";
+            }
             
             else if (position[i][j]["Offen"] === false) {
                 img.src = "./img/tile.png";
             }
 
             else {
-                img.src = `./img/sweepertile${position[i][j]["tiles"]}.png`;
+                
             }
 
             createDiv.appendChild(img);
@@ -104,3 +134,4 @@ function generateField(position) {
 }
 
 generateField(exPosition)
+
