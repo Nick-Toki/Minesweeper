af20fb919b73d0f53aef35205ebf9e8a36cd336a
Nick-Toki <nickstoll1@gmx.de>
2024-07-09 11:15:26 +0200

    update main

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,26 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    field = Minesweeper.create_field(2, 1, 10, 20, ["1", "2", "3", "0"])
+    field = Minesweeper.create_field(2, 1, 10, 20, ["1", "2", "3" , "0"])
+    return Response(json.dumps(field), mimetype='application/json')
+
+
+@app.route('/update')
+def update():
+    var = request.args.get("var")
+
+    return var
+
+
+app.run(host='0.0.0.0', port=81)
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    field = Minesweeper.aufdecken(field)
+    
     return Response(json.dumps(field), mimetype='application/json')
 
 
